e98121f5d1b69aa300aa15b9693bc36c499a6e19
preciousimo <preciousimoniakemu@gmail.com>
2023-04-18 16:19:05 +0100

    Create ReactJs Functional Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <h3>WELCOME TO MEADOW HALL SCHOOLS!!</h3>
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <h3>This is the Home component!!</h3>
+    </div>
+  )
+}
+export default Home;
