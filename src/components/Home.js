1ecfc9c847b416097b499579734e340e9f420428
preciousimo <preciousimoniakemu@gmail.com>
2023-04-21 13:01:57 +0100

    Create Home page Layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,8 @@ function Home() {
     <div className='container mt-4'>
       <h1 className='text-center'>Welcome to Meadow Hall</h1>
       {/* Latest subjects */}
-      <h3 className='pb-1 mb-4'>Latest Subjects</h3>
-      <div className='row'>
+      <h3 className='pb-1 mb-4'>Latest Subjects <a href=' ' className='float-end'>See All</a></h3>
+      <div className='row mb-4'>
         <div className='col-md-3'>
           <div className='card' style={{width: '18rem'}} >
             <a href=' '>
@@ -49,9 +49,10 @@ function Home() {
         </div>
       </div>
       {/* End latest subjects */}
+
       {/* Top subjects */}
-      <h3 className='pb-1 mb-4 mt-5'>Top Subjects</h3>
-      <div className='row'>
+      <h3 className='pb-1 mb-4 mt-5'>Top Subjects <a href=' ' className='float-end'>See All</a></h3>
+      <div className='row mb-4'>
         <div className='col-md-3'>
           <div className='card' style={{width: '18rem'}} >
             <a href=' '>
@@ -94,6 +95,103 @@ function Home() {
         </div>
       </div>
       {/* End top subjects */}
+
+      {/* Featured teachers */}
+      <h3 className='pb-1 mb-4 mt-5'>Featured Teachers <a href=' ' className='float-end'>See All</a></h3>
+      <div className='row mb-4'>
+        <div className='col-md-3'>
+          <div className='card' style={{width: '18rem'}} >
+            <a href=' '>
+              <img src='https://images.unsplash.com/photo-1612477954469-c6a60de89802?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80' className='card-img-top' alt='...' />
+            </a>
+            <div className='card-body'>
+              <h5 className='card-title'><a href=' '>Mr Precious</a></h5>
+            </div>
+          </div>
+        </div>
+        <div className='col-md-3'>
+          <div className='card' style={{width: '18rem'}} >
+            <a href=' '>
+              <img src='https://images.unsplash.com/photo-1612477954469-c6a60de89802?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80' className='card-img-top' alt='...' />
+            </a>
+            <div className='card-body'>
+              <h5 className='card-title'><a href=' '>Mr Imoniakemu</a></h5>
+            </div>
+          </div>
+        </div>
+        <div className='col-md-3'>
+          <div className='card' style={{width: '18rem'}} >
+            <a href=' '>
+              <img src='https://images.unsplash.com/photo-1612477954469-c6a60de89802?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80' className='card-img-top' alt='...' />
+            </a>
+            <div className='card-body'>
+              <h5 className='card-title'><a href=' '>Mr Ayeni</a></h5>
+            </div>
+          </div>
+        </div>
+        <div className='col-md-3'>
+          <div className='card' style={{width: '18rem'}} >
+            <a href=' '>
+              <img src='https://images.unsplash.com/photo-1612477954469-c6a60de89802?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80' className='card-img-top' alt='...' />
+            </a>
+            <div className='card-body'>
+              <h5 className='card-title'><a href=' '>Mr Adegoke</a></h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Featured teachers */}
+
+      {/* Student testimonial */}
+      <h3 className='pb-1 mb-4 mt-5'>Student Testimonial</h3>
+        <div id="carouselExampleIndicators" className="carousel slide bg-dark text-white py-5">
+          <div className="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          </div>
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+            <figure class="text-center">
+              <blockquote class="blockquote">
+                <p>A well-known quote, contained in a blockquote element.</p>
+              </blockquote>
+              <figcaption class="blockquote-footer">
+                Someone famous in <cite title="Source Title">Source Title</cite>
+              </figcaption>
+            </figure>
+            </div>
+            <div className="carousel-item">
+            <figure class="text-center">
+              <blockquote class="blockquote">
+                <p>A well-known quote, contained in a blockquote element.</p>
+              </blockquote>
+              <figcaption class="blockquote-footer">
+                Someone famous in <cite title="Source Title">Source Title</cite>
+              </figcaption>
+            </figure>
+            </div>
+            <div className="carousel-item">
+            <figure class="text-center">
+              <blockquote class="blockquote">
+                <p>A well-known quote, contained in a blockquote element.</p>
+              </blockquote>
+              <figcaption class="blockquote-footer">
+                Someone famous in <cite title="Source Title">Source Title</cite>
+              </figcaption>
+            </figure>
+            </div>
+          </div>
+          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+      {/* Student testimonial */}
     </div>
   )
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Home from './Home';
 import Header from './Header';
+import Footer from './Footer';
 
 function Main() {
   return (
     <div>
         <Header />
         <Home />
+        <Footer />
     </div>
   )
 }
