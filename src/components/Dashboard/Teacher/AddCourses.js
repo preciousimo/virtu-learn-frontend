0c5c91cfc0bec833f23d33f03666c5a87a2b8d47
preciousimo <preciousimoniakemu@gmail.com>
2024-01-08 22:11:41 +0100

    feat: Add teacher course upload component in React

--- src/components/Dashboard/Teacher/AddCourses.js
+++ src/components/Dashboard/Teacher/AddCourses.js
@@ -1,11 +1,75 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import TeacherSidebar from './TeacherSidebar'
+import axios from 'axios'
+
+const baseUrl = 'http://127.0.0.1:8000/api';
 
 function AddCourses() {
     useEffect(() => {
-        document.title = 'Add Subjects'
-    }, [])
+        document.title = 'Add Courses';
+    }, []);
+
+    const [cats, setCats] = useState([]);
+    const [courseData, setCourseData] = useState({
+        category: '',
+        title: '',
+        description: '',
+        f_img: '',
+        techs: '',
+    });
+
+    useEffect(() => {
+        try {
+            axios.get(`${baseUrl}/category/`).then((res) => {
+                setCats(res.data);
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    }, []);
+
+    const handleChange = (e) => {
+        setCourseData({
+            ...courseData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleFileChange = (e) => {
+        setCourseData({
+            ...courseData,
+            [e.target.name]: e.target.files[0],
+        });
+    };
+
+    const formSubmit = () => {
+        const _formData = new FormData();
+        _formData.append('category', courseData.category);
+        _formData.append('teacher', 1);
+        _formData.append('title', courseData.title);
+        _formData.append('description', courseData.description);
+        _formData.append('featured_img', courseData.f_img, courseData.f_img.name);
+        _formData.append('techs', courseData.techs);
+
+        try {
+            axios.post(`${baseUrl}/course/`, _formData, {
+                headers: {
+                    'content-type': 'multipart/form-data',
+                },
+            }).then((res) => {
+                console.log(res.data);
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        formSubmit();
+    };
+
     return (
         <div className='container mt-4'>
             <div className='row'>
@@ -14,33 +78,69 @@ function AddCourses() {
                 </aside>
                 <section className='col-md-9'>
                     <div className='card'>
-                        <h5 className='card-header'>Add Subjects</h5>
+                        <h5 className='card-header'>Add Course</h5>
                         <div className='card-body'>
-                            <form>
+                            <form onSubmit={handleSubmit}>
                                 <div className="mb-3">
-                                    <label for="title" className="form-label">Title</label>
-                                    <input type="text" className="form-control" id="title" />
+                                    <label htmlFor="category" className="form-label">Category</label>
+                                    <select name='category' className='form-control' onChange={handleChange}>
+                                        {cats.map((category, index) => (
+                                            <option key={index} value={category.id}>
+                                                {category.title}
+                                            </option>
+                                        ))}
+                                    </select>
                                 </div>
                                 <div className="mb-3">
-                                    <label for="description" className="form-label">Description</label>
-                                    <textarea className="form-control" id="description" rows="3"></textarea>
+                                    <label htmlFor="title" className="form-label">Title</label>
+                                    <input
+                                        type='text'
+                                        className="form-control"
+                                        id="title"
+                                        name="title"
+                                        onChange={handleChange}
+                                        rows="3"
+                                    />
                                 </div>
                                 <div className="mb-3">
-                                    <label for="video" className="form-label">Subject Video</label>
-                                    <input type="file" className="form-control" id="video" />
+                                    <label htmlFor="description" className="form-label">Description</label>
+                                    <textarea
+                                        className="form-control"
+                                        id="description"
+                                        name="description"
+                                        value={courseData.description}
+                                        onChange={handleChange}
+                                    ></textarea>
                                 </div>
                                 <div className="mb-3">
-                                    <label for="technologies" className="form-label">Technologies</label>
-                                    <textarea className="form-control" id="description" rows="3"></textarea>
+                                    <label htmlFor="f_img" className="form-label">Featured Image</label>
+                                    <input
+                                        type="file"
+                                        className="form-control"
+                                        id="f_img"
+                                        name="f_img"
+                                        onChange={handleFileChange}
+                                    />
                                 </div>
-                                <button className='btn btn-secondary'>Submit</button>
+                                <div className="mb-3">
+                                    <label htmlFor="technologies" className="form-label">Technologies</label>
+                                    <textarea
+                                        className="form-control"
+                                        id="technologies"
+                                        name="techs"
+                                        value={courseData.techs}
+                                        onChange={handleChange}
+                                        rows="3"
+                                    ></textarea>
+                                </div>
+                                <button type='submit' className='btn btn-secondary'>Submit</button>
                             </form>
                         </div>
                     </div>
                 </section>
             </div>
         </div>
-    )
+    );
 }
 
-export default AddCourses
+export default AddCourses;
