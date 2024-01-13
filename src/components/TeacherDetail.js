cb60d41353f206d1f9347bebdb38fa35afb3c304
preciousimo <preciousimoniakemu@gmail.com>
2024-01-13 22:03:15 +0100

    feat: Create Teacher Detail Page with Course List

--- src/components/TeacherDetail.js
+++ src/components/TeacherDetail.js
@@ -1,10 +1,30 @@
-import { useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { Link, useParams } from 'react-router-dom';
+import axios from 'axios'
+
+const baseUrl = 'http://127.0.0.1:8000/api';
 
 function TeacherDetail() {
     useEffect(() => {
         document.title = 'Teacher Detail'
     }, [])
+
+    const [teacherData, setTeacherData] = useState([]);
+    const [courseData, setCourseData] = useState([]);
+    let { teacher_id } = useParams();
+
+    useEffect(() => {
+        try {
+            axios.get(`${baseUrl}/teacher/${teacher_id}`)
+                .then((res) => {
+                    setTeacherData(res.data);
+                    setCourseData(res.data.teacher_courses);
+                });
+        } catch (err) {
+            console.error(err);
+        }
+    }, [teacher_id]);
+
     return (
         <div className='container mt-3'>
             <div className='row'>
@@ -12,8 +32,8 @@ function TeacherDetail() {
                     <img src='https://images.unsplash.com/photo-1612477954469-c6a60de89802?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80' className='card-img-top' alt='Teacher' />
                 </div>
                 <div className='col-8'>
-                    <h3>Mr Precious</h3>
-                    <p>lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies tincidunt, nunc nisl aliquam nisl, eget aliquam ni</p>
+                    <h3>{teacherData.name}</h3>
+                    <p>{teacherData.detail}</p>
                     <p className='fw-bold'>Skills: <Link to='/category/python'>Python</Link>, <Link to='/category/java'>JavaScript</Link>, <Link to='/category/php'>PHP</Link></p>
                     <p className='fw-bold'>Recent Subject: <Link to='/category/django'>Django Course</Link></p>
                     <p className='fw-bold'>Rating: <a href=' '>4.5/5</a></p>
@@ -25,12 +45,9 @@ function TeacherDetail() {
                     <h5>Subject List</h5>
                 </div>
                 <div className='list-group list-group-flush'>
-                    <Link to="/subject-detail/1" className='list-group-item list-group-item-action'>Python Course 1</Link>
-                    <Link to="/subject-detail/1" className='list-group-item list-group-item-action'>Python Course 2</Link>
-                    <Link to="/subject-detail/1" className='list-group-item list-group-item-action'>JavaScript Course 1</Link>
-                    <Link to="/subject-detail/1" className='list-group-item list-group-item-action'>JavaScript Course 2</Link>
-                    <Link to="/subject-detail/1" className='list-group-item list-group-item-action'>PHP Course 1</Link>
-                    <Link to="/subject-detail/1" className='list-group-item list-group-item-action'>PHP Course 2</Link>
+                    {Array.isArray(courseData) && courseData.map((course, index) => (
+                        <Link to={`/detail/${course.id}`} className='list-group-item list-group-item-action'>{course.title}</Link>
+                    ))}
                 </div>
             </div>
 
