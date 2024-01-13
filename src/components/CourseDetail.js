fb36f725b80aea742ab2fa67e464d9fc93b0883a
preciousimo <preciousimoniakemu@gmail.com>
2024-01-13 15:06:28 +0100

    feat: Fetch Course Detail and Chapters

--- src/components/CourseDetail.js
+++ src/components/CourseDetail.js
@@ -1,21 +1,39 @@
-import {useEffect} from 'react'
-import { Link } from 'react-router-dom'
+import {useState, useEffect} from 'react'
+import { Link, useParams } from 'react-router-dom';
+import axios from 'axios'
+
+const baseUrl = 'http://127.0.0.1:8000/api';
 
 function CourseDetail() {
     useEffect(() => {
-        document.title = 'Subject Detail'
+        document.title = 'Course Detail'
     }, [])
-    // let { subject_id } = useParams();
+
+    const [courseData, setCourseData] = useState([]);
+    const [chapterData, setChapterData] = useState([]);
+    let { course_id } = useParams();
+
+    useEffect(() => {
+        try {
+            axios.get(`${baseUrl}/course/${course_id}`)
+                .then((res) => {
+                    setCourseData(res.data);
+                });
+        } catch (err) {
+            console.error(err);
+        }
+    }, [course_id]);
+    
     return (
         <div className='container mt-3'>
             <div className='row'>
                 <div className='col-4'>
-                    <img src='https://images.unsplash.com/photo-1612477954469-c6a60de89802?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80' className='card-img-top' alt='...' />
+                    <img src={courseData.featured_img} className='card-img-top' alt={courseData.title} />
                 </div>
                 <div className='col-8'>
-                    <h3>Subject Title</h3>
-                    <p>lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies tincidunt, nunc nisl aliquam nisl, eget aliquam ni</p>
-                    <p className='fw-bold'>Subject By: <Link to='/teacher-detail/1'>Mr Precious</Link></p>
+                    <h3>{courseData.title}</h3>
+                    <p>{courseData.description}</p>
+                    <p className='fw-bold'>Subject By: {courseData.teacher ? <Link to={`/teacher-detail/${courseData.teacher.id}`}>{courseData.teacher.name}</Link> : 'Unknown Teacher'}</p>
                     <p className='fw-bold'>Duration: <a href=' '>3 Hours 30 Minutes</a></p>
                     <p className='fw-bold'>Total Enrolled: <a href=' '>456 Student</a></p>
                     <p className='fw-bold'>Rating: <a href=' '>4.5/5</a></p>
@@ -27,7 +45,8 @@ function CourseDetail() {
                     <h5>Subject Videos</h5>
                 </div>
                 <ul className='list-group list-group-flush'>
-                    <li className='list-group-item'>Video 1
+                {Array.isArray(chapterData) && chapterData.map((chapter, index) => (
+                    <li className='list-group-item'>{chapter.title} 
                         <span className='float-end'>
                             <button className='btn btn-sm'>
                                 <span className='me-3'>1 Hour 30 Minutes</span>
@@ -44,50 +63,19 @@ function CourseDetail() {
                                     </div>
                                     <div className="modal-body">
                                         <div class="ratio ratio-16x9">
-                                            <iframe src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" title="YouTube video" allowfullscreen></iframe>
+                                            <iframe src={chapter.video} title={chapter.title} allowfullscreen></iframe>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </li>
-                    <li className='list-group-item'>Video 2
-                        <span className='float-end'>
-                            <button className='btn btn-sm'>
-                                <span className='me-3'>1 Hour 30 Minutes</span>
-                                <i className="fa-sharp fa-solid fa-circle-play fa-2xl" style={{color: '#d40c0c'}}></i>
-                            </button>
-                        </span>
-                    </li>
-                    <li className='list-group-item'>Video 3
-                        <span className='float-end'>
-                            <button className='btn btn-sm'>
-                                <span className='me-3'>1 Hour 30 Minutes</span>
-                                <i className="fa-sharp fa-solid fa-circle-play fa-2xl" style={{color: '#d40c0c'}}></i>
-                            </button>
-                        </span>
-                    </li>
-                    <li className='list-group-item'>Video 4
-                        <span className='float-end'>
-                            <button className='btn btn-sm'>
-                                <span className='me-3'>1 Hour 30 Minutes</span>
-                                <i className="fa-sharp fa-solid fa-circle-play fa-2xl" style={{color: '#d40c0c'}}></i>
-                            </button>
-                        </span>
-                    </li>
-                    <li className='list-group-item'>Video 5
-                        <span className='float-end'>
-                            <button className='btn btn-sm'>
-                                <span className='me-3'>1 Hour 30 Minutes</span>
-                                <i className="fa-sharp fa-solid fa-circle-play fa-2xl" style={{color: '#d40c0c'}}></i>
-                            </button>
-                        </span>
-                    </li>
+                ))}
                 </ul>
             </div>
 
             {/* Related Subjects */}
-            <h3 className='pb-1 mb-4 mt-5'>Related Subjects</h3>
+            <h3 className='pb-1 mb-4 mt-5'>Related Courses</h3>
             <div className="row row-cols-1 row-cols-sm-2 row-cols-md-4 g-4">
                 <div className="col">
                     <div className="card h-100">
