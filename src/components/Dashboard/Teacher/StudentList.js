847b80a5c8c8731c1dfc57b87e3d896161f3b001
preciousimo <preciousimoniakemu@gmail.com>
2024-01-17 09:53:22 +0100

    feat: Display All Enrolled Students on Teacher Dashboard

--- src/components/Dashboard/Teacher/StudentList.js
+++ src/components/Dashboard/Teacher/StudentList.js
@@ -1,11 +1,29 @@
-import { useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { Link, useParams } from 'react-router-dom'
 import TeacherSidebar from './TeacherSidebar'
+import axios from 'axios'
+
+const baseUrl = 'http://127.0.0.1:8000/api';
 
 function StudentList() {
     useEffect(() => {
-        document.title = 'Student List'
+        document.title = 'Enrolled Students'
     }, [])
+
+    const [studentData, setStudentData]=useState([]);
+    const teacherId = localStorage.getItem('teacherId');
+
+    useEffect(() => {
+        try {
+            axios.get(`${baseUrl}/fetch-all-enrolled-students/${teacherId}`)
+            .then((res) => {
+                setStudentData(res.data);
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    }, []);
+
     return (
         <div className='container mt-4'>
             <div className='row'>
@@ -14,26 +32,27 @@ function StudentList() {
                 </aside>
                 <section className='col-md-9'>
                     <div className='card'>
-                        <h5 className='card-header'>Student List</h5>
+                        <h5 className='card-header'>All Student List</h5>
                         <div className='card-body'>
                             <table className='table table-bordered'>
                                 <thead>
                                     <tr>
                                         <th>Name</th>
-                                        <th>Enrolled Course</th>
-                                        <th>Action</th>
+                                        <th>Email</th>
+                                        <th>Username</th>
+                                        <th>Interested Categories</th>
                                     </tr>
                                 </thead>
                                 <tbody>
+                                    {studentData.map((row, index) => (
                                     <tr>
-                                        <td>John Doe</td>
-                                        <td><Link to="/">3</Link></td>
-                                        <td>
-                                            <a href='#' className='btn btn-sm btn-primary'>View</a>
-                                            <a href='#' className='btn btn-sm btn-secondary'>Edit</a>
-                                            <a href='#' className='btn btn-sm btn-danger'>Delete</a>
-                                        </td>
+                                        <td>{row.student.name}</td>
+                                        <td>{row.student.email}</td>
+                                        <td>{row.student.username}</td>
+                                        <td>{row.student.interested_categories}</td>
                                     </tr>
+                                    )
+                                    )}
                                 </tbody>
                             </table>
                         </div>
