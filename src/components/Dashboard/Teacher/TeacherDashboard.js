7f37c605994aab7034a626a521e1d0103ece0812
preciousimo <preciousimoniakemu@gmail.com>
2024-01-17 14:12:44 +0100

    feat: Display Analytics Data on Teacher Dashboard

--- src/components/Dashboard/Teacher/TeacherDashboard.js
+++ src/components/Dashboard/Teacher/TeacherDashboard.js
@@ -1,21 +1,64 @@
-import {useEffect} from 'react'
-import { Link } from 'react-router-dom' 
+import { useState, useEffect } from 'react'
+import { Link, useParams } from 'react-router-dom';
 import TeacherSidebar from './TeacherSidebar'
+import axios from 'axios'
+import Swal from 'sweetalert2'
+
+const baseUrl = 'http://127.0.0.1:8000/api';
 
 function TeacherDashboard() {
   useEffect(() => {
     document.title = 'Teacher Dashboard'
   }, [])
+
+  const [dashboardData, setDashboardData] = useState([]);
+  const teacherId = localStorage.getItem('teacherId');
+
+  useEffect(() => {
+    try {
+      axios.get(`${baseUrl}/teacher/dashboard/${teacherId}/`, teacherId)
+        .then((res) => {
+          setDashboardData(res.data);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  })
   return (
     <div className='container mt-4'>
-        <div className='row'>
-          <aside className='col-md-3'>
-            <TeacherSidebar/>
-          </aside>
-          <section className='col-md-9'>
-              Teacher Dashboard
-          </section>
-        </div>
+      <div className='row'>
+        <aside className='col-md-3'>
+          <TeacherSidebar />
+        </aside>
+        <section className='col-md-9'>
+          <div className='row'>
+            <div className='col-md-4'>
+              <div className='card border-primary'>
+                <h3 className='card-header bg-primary text-white'>Total Courses</h3>
+                <div className='card-body'>
+                  <h3><Link to='/teacher-courses'>{dashboardData.total_teacher_courses}</Link></h3>
+                </div>
+              </div>
+            </div>
+            <div className='col-md-4'>
+              <div className='card border-success'>
+                <h3 className='card-header bg-success text-white'>Total Students</h3>
+                <div className='card-body'>
+                  <h3><Link to='/teacher-students'>{dashboardData.total_teacher_students}</Link></h3>
+                </div>
+              </div>
+            </div>
+            <div className='col-md-4'>
+              <div className='card border-info'>
+                <h3 className='card-header bg-info text-white'>Total Chapters</h3>
+                <div className='card-body'>
+                  <h3><Link to='/teacher-courses'>{dashboardData.total_teacher_chapters}</Link></h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   )
 }
