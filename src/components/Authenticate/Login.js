e24107b09dee1bd35b9d645b19142318fe2e3428
preciousimo <preciousimoniakemu@gmail.com>
2024-01-31 14:27:00 +0100

    feat: Add "Remember Me" Checkbox to Login Form and Option to View Entered Password

--- src/components/Authenticate/Login.js
+++ src/components/Authenticate/Login.js
@@ -1,25 +1,43 @@
-import { Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const baseUrl = 'http://127.0.0.1:8000/api';
+
 function Login() {
-  useEffect(() => {
-    document.title = 'Login'
-  }, [])
 
   const [studentLogin, setStudentLogin] = useState({
     email: '',
     password: '',
+    rememberMe: false
   });
 
   const [errorMsg, setErrorMsg] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  useEffect(() => {
+    document.title = 'Login'
+    const storedEmail = localStorage.getItem('email');
+    const storedPassword = localStorage.getItem('password');
+    if (storedEmail && storedPassword) {
+      setStudentLogin({
+        ...studentLogin,
+        email: storedEmail,
+        password: storedPassword
+      });
+    }
+  }, []);
 
   const handleChange = (e) => {
-    setStudentLogin({
-      ...studentLogin,
-      [e.target.name]: e.target.value
-    })
+    const { name, value, checked, type } = e.target;
+
+    setStudentLogin(prevState => ({
+      ...prevState,
+      [name]: type === 'checkbox' ? checked : value
+    }));
+  }
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(prevState => !prevState);
   }
 
   const submitForm = async () => {
@@ -33,6 +51,13 @@ function Login() {
       if (res.data.bool === true) {
         localStorage.setItem('studentLoginStatus', true);
         localStorage.setItem('studentId', res.data.student_id);
+        if (studentLogin.rememberMe) {
+          localStorage.setItem('email', studentLogin.email);
+          localStorage.setItem('password', studentLogin.password);
+        } else {
+          localStorage.removeItem('email');
+          localStorage.removeItem('password');
+        }
         window.location.href = '/';
       } else {
         setErrorMsg('Invalid credentials');
@@ -42,11 +67,6 @@ function Login() {
     }
   };
 
-
-  const studentLoginStatus = localStorage.getItem('studentLoginStatus');
-  if (studentLoginStatus === 'true') {
-    window.location.href = '/';
-  }
   return (
     <div className="container">
       <div className="row justify-content-center mt-5">
@@ -56,22 +76,25 @@ function Login() {
               <h4>Login</h4>
             </div>
             <div className="card-body">
-              <div className="card-body">
-                {errorMsg && <p className='text-danger'>{errorMsg}</p>}
-                <div className="mb-3">
-                  <label for="email" className="form-label">Email address</label>
-                  <input type="email" name='email' onChange={handleChange} value={studentLogin.email} className="form-control" />
-                </div>
-                <div className="mb-3">
-                  <label for="password" className="form-label">Password</label>
-                  <input type="password" name='password' onChange={handleChange} value={studentLogin.password} className="form-control" id="password" placeholder="Password" />
-                </div>
-                {/* <div className="form-check mb-3">
-                  <input type="checkbox" className="form-check-input" id="rememberMe" />
-                  <label className="form-check-label" for="rememberMe">Remember me</label>
-                </div> */}
-                <button type="submit" onClick={submitForm} className="btn btn-secondary">Login</button>
+              {errorMsg && <p className='text-danger'>{errorMsg}</p>}
+              <div className="mb-3">
+                <label htmlFor="email" className="form-label">Email address</label>
+                <input type="email" name='email' onChange={handleChange} value={studentLogin.email} className="form-control" />
               </div>
+              <div className="mb-3">
+                <label htmlFor="password" className="form-label">Password</label>
+                <div className="input-group">
+                  <input type={showPassword ? "text" : "password"} name='password' onChange={handleChange} value={studentLogin.password} className="form-control" id="password" placeholder="Password" />
+                  <button className="btn btn-outline-secondary" type="button" onClick={togglePasswordVisibility}>
+                    {showPassword ? "Hide" : "Show"}
+                  </button>
+                </div>
+              </div>
+              <div className="form-check mb-3">
+                <input type="checkbox" className="form-check-input" id="rememberMe" name="rememberMe" onChange={handleChange} checked={studentLogin.rememberMe} />
+                <label className="form-check-label" htmlFor="rememberMe">Remember me</label>
+              </div>
+              <button type="submit" onClick={submitForm} className="btn btn-secondary">Login</button>
             </div>
           </div>
         </div>
@@ -80,4 +103,4 @@ function Login() {
   )
 }
 
-export default Login
+export default Login;
