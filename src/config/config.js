e50f68212aa9c10ed7c102cc4325dbf8fa441975
preciousimo <preciousimoniakemu@gmail.com>
2024-02-22 06:44:58 +0100

    feat: Add configuration for development and production environments

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,14 @@
-const config = {
+const development = {
+    baseUrl: "http://127.0.0.1:8000/api",
+    siteUrl: 'https://myfundz.s3.amazonaws.com',
+  };
+  
+  const production = {
     baseUrl: 'https://virtulearn-api.onrender.com/api',
     siteUrl: 'https://myfundz.s3.amazonaws.com',
   };
   
-export default config;
+  const config = process.env.NODE_ENV === 'development' ? development : production;
+  
+  export default config;
+  
